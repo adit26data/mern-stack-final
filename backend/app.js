1f3e86e3b898107abc6cbc88cb51ed23f5bcebd4
Adit Kaushal <98691664+adit26data@users.noreply.github.com>
2023-06-10 22:13:14 +0530

    adding stripe

--- backend/app.js
+++ backend/app.js
@@ -5,19 +5,24 @@ const errorMiddleware = require('./middleware/error');
 const bodyParser = require('body-parser');
 
 const fileUpload = require('express-fileupload')
+const dotenv = require('dotenv')
+dotenv.config({ path: "backend/config/config.env" });
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
+
 //route imports 
 const product = require('./routes/productRoute');
 const user = require('./routes/userRoute');
-const order = require('./routes/orderRoutes')
+const order = require('./routes/orderRoutes');
+const payment = require("./routes/paymentRoute")
 //route imports
 app.use('/api/v1', product);
 app.use('/api/v1', user);
 app.use('/api/v1', order);
+app.use('/api/v1', payment)
 
 
 //middleware for errors
