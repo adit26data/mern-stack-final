893640e82061e9fbfd9e8a70baa813e8c08124b7
Adit Kaushal <kaushaladit26@gmail.com>
2023-09-03 19:11:55 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ import Home from "./component/Home/Home.js"
 import ProductDetails from './component/Product/ProductDetails.js';
 import Products from "./component/Product/Products.js"
 import Search from "./component/Product/Search.js"
-import LoginSignUp from './component/User/LoginSignUp';
+import LoginSignUp from './component/User/LoginSignup';
 import store from "./store"
 import { loadUser } from './actions/userAction';
 import UserOptions from "./component/layout/Header/UserOptions.js"
